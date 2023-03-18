16fb4ebde9043a29dae74a13ea2669b3898a34e0
Liu Zeyu <12111208.ststech.edu.cn>
2023-03-18 23:59:44 +0800

    修改格式，当前style错误减少至276个，基本上全是缩进，我不是很习惯只有两格的缩进，所以就没改。

--- src/OnlineCoursesAnalyzer.java
+++ src/OnlineCoursesAnalyzer.java
@@ -8,10 +8,12 @@ import java.util.stream.Collectors;
 
 /**
  *
- * This is just a demo for you, please run it on JDK17 (some statements may be not allowed in lower version).
+ * This is just a demo for you, please run it on JDK17
+ * (some statements may be not allowed in lower version).
  * This is just a demo, and you can extend and implement functions
  * based on this demo, or implement it in a different way.
  */
+
 public class OnlineCoursesAnalyzer {
 
     List<Course> courses = new ArrayList<>();
@@ -19,6 +21,7 @@ public class OnlineCoursesAnalyzer {
     public static void main(String[] args) {
         OnlineCoursesAnalyzer test = new OnlineCoursesAnalyzer("resources/local.csv");
     }
+
     public OnlineCoursesAnalyzer(String datasetPath) {
         BufferedReader br = null;
         String line;
@@ -28,12 +31,23 @@ public class OnlineCoursesAnalyzer {
             while ((line = br.readLine()) != null) {
                 String[] info = line.split(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)", -1);
                 Course course = new Course(info[0], info[1], new Date(info[2]), info[3], info[4], info[5],
-                        Integer.parseInt(info[6]), Integer.parseInt(info[7]), Integer.parseInt(info[8]),
-                        Integer.parseInt(info[9]), Integer.parseInt(info[10]), Double.parseDouble(info[11]),
-                        Double.parseDouble(info[12]), Double.parseDouble(info[13]), Double.parseDouble(info[14]),
-                        Double.parseDouble(info[15]), Double.parseDouble(info[16]), Double.parseDouble(info[17]),
-                        Double.parseDouble(info[18]), Double.parseDouble(info[19]), Double.parseDouble(info[20]),
-                        Double.parseDouble(info[21]), Double.parseDouble(info[22]));
+                        Integer.parseInt(info[6]),
+                        Integer.parseInt(info[7]),
+                        Integer.parseInt(info[8]),
+                        Integer.parseInt(info[9]),
+                        Integer.parseInt(info[10]),
+                        Double.parseDouble(info[11]),
+                        Double.parseDouble(info[12]),
+                        Double.parseDouble(info[13]),
+                        Double.parseDouble(info[14]),
+                        Double.parseDouble(info[15]),
+                        Double.parseDouble(info[16]),
+                        Double.parseDouble(info[17]),
+                        Double.parseDouble(info[18]),
+                        Double.parseDouble(info[19]),
+                        Double.parseDouble(info[20]),
+                        Double.parseDouble(info[21]),
+                        Double.parseDouble(info[22]));
                 courses.add(course);
             }
         } catch (IOException e) {
@@ -60,18 +74,19 @@ public class OnlineCoursesAnalyzer {
 
     //2
     public Map<String, Integer> getPtcpCountByInstAndSubject() {
-        Map<String, Integer> result = new LinkedHashMap<String, Integer>();
+        Map<String, Integer> result = new LinkedHashMap<>();
         for (Course course : courses) {
-            String InstAndSub = course.institution + "-" + course.subject;
-            result.merge(InstAndSub, course.participants, Integer::sum);
+            String instAndSub = course.institution + "-" + course.subject;
+            result.merge(instAndSub, course.participants, Integer::sum);
         }
-        result = result.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+        result = result.entrySet().stream()
+            .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
             .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (s, s2) -> s,
                 LinkedHashMap::new));
-        result = result.entrySet().stream().sorted((e1,e2)-> {
+        result = result.entrySet().stream().sorted((e1, e2) -> {
             if (e1.getValue().equals(e2.getValue())) {
                 return e1.getKey().compareTo(e2.getKey());
-            }else {
+            } else {
                 return 0;
             }
             })
@@ -84,29 +99,29 @@ public class OnlineCoursesAnalyzer {
     public Map<String, List<List<String>>> getCourseListOfInstructor() {
         Map<String, List<List<String>>> result = new LinkedHashMap<>();
         for (Course course : courses) {
-            String[] Instructors = course.instructors.split(", ");
-            if (Instructors.length == 1) {
-                if (result.containsKey(Instructors[0])) {
-                    result.get(Instructors[0]).get(0).add(course.title);
+            String[] instructors = course.instructors.split(", ");
+            if (instructors.length == 1) {
+                if (result.containsKey(instructors[0])) {
+                    result.get(instructors[0]).get(0).add(course.title);
                 } else {
                     List<String> help1 = new ArrayList<>();
                     help1.add(course.title);
                     List<List<String>> res = new ArrayList<>();
                     res.add(help1);
                     res.add(new ArrayList<>());
-                    result.put(Instructors[0], res);
+                    result.put(instructors[0], res);
                 }
-            } else if (Instructors.length > 1) {
-                for (String Instructor : Instructors) {
-                    if (result.containsKey(Instructor)) {
-                        result.get(Instructor).get(1).add(course.title);
+            } else if (instructors.length > 1) {
+                for (String instructor : instructors) {
+                    if (result.containsKey(instructor)) {
+                        result.get(instructor).get(1).add(course.title);
                     } else {
                         List<String> help1 = new ArrayList<>();
                         help1.add(course.title);
                         List<List<String>> res = new ArrayList<>();
                         res.add(new ArrayList<>());
                         res.add(help1);
-                        result.put(Instructor, res);
+                        result.put(instructor, res);
                     }
                 }
             }
@@ -131,18 +146,20 @@ public class OnlineCoursesAnalyzer {
             Map<String, Double> titleTime = new LinkedHashMap<>();
             for (Course course : courses) {
                 if (titleTime.containsKey(course.title)) {
-                    titleTime.put(course.title, Math.max(titleTime.get(course.title),course.totalHours));
+                    titleTime.put(course.title,
+                        Math.max(titleTime.get(course.title), course.totalHours));
                 } else {
                     titleTime.put(course.title, course.totalHours);
                 }
             }
-            titleTime = titleTime.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+            titleTime = titleTime.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (s, s2) -> s,
                     LinkedHashMap::new));
-            stringList = titleTime.entrySet().stream().sorted((e1,e2)-> {
+            stringList = titleTime.entrySet().stream().sorted((e1, e2) -> {
                     if (e1.getValue().equals(e2.getValue())) {
                         return e1.getKey().compareTo(e2.getKey());
-                    }else {
+                    } else {
                         return 0;
                     }
                 })
@@ -152,18 +169,19 @@ public class OnlineCoursesAnalyzer {
             Map<String, Integer> titlePart = new LinkedHashMap<>();
             for (Course course : courses) {
                 if (titlePart.containsKey(course.title)) {
-                    titlePart.put(course.title, Math.max(titlePart.get(course.title),course.participants));
+                    titlePart.put(course.title, Math.max(titlePart.get(course.title), course.participants));
                 } else {
                     titlePart.put(course.title, course.participants);
                 }
             }
-            titlePart = titlePart.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+            titlePart = titlePart.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (s, s2) -> s,
                     LinkedHashMap::new));
-            stringList = titlePart.entrySet().stream().sorted((e1,e2)-> {
+            stringList = titlePart.entrySet().stream().sorted((e1, e2) -> {
                     if (e1.getValue().equals(e2.getValue())) {
                         return e1.getKey().compareTo(e2.getKey());
-                    }else {
+                    } else {
                         return 0;
                     }
                 })
@@ -197,12 +215,12 @@ public class OnlineCoursesAnalyzer {
         Map<String, Date> idDateTable = new LinkedHashMap<>();
         for (Course course : courses) {
             if (!idInfoTable.containsKey(course.number)) {
-                List<Double> Info = new ArrayList<>();
-                Info.add(course.medianAge);
-                Info.add(course.percentMale);
-                Info.add(course.percentDegree);
-                Info.add(1d);
-                idInfoTable.put(course.number, Info);
+                List<Double> info = new ArrayList<>();
+                info.add(course.medianAge);
+                info.add(course.percentMale);
+                info.add(course.percentDegree);
+                info.add(1d);
+                idInfoTable.put(course.number, info);
             } else {
                 List<Double> helper = idInfoTable.get(course.number);
                 idInfoTable.get(course.number).set(0, helper.get(0) + course.medianAge);
@@ -223,10 +241,11 @@ public class OnlineCoursesAnalyzer {
         Map<String, Double> idSimTable = new LinkedHashMap<>();
         for (Entry<String, List<Double>> entry : idInfoTable.entrySet()) {
             List<Double> info = entry.getValue();
-            double averageAge = (double)info.get(0) / info.get(3);
+            double averageAge = (double) info.get(0) / info.get(3);
             double averageMale = (double) info.get(1) / info.get(3);
             double averageBachelor = (double) info.get(2) / info.get(3);
-            double similarityvalue = Math.pow(age - averageAge, 2) + Math.pow(gender * 100 - averageMale, 2)
+            double similarityvalue = Math.pow(age - averageAge, 2)
+                + Math.pow(gender * 100 - averageMale, 2)
                 + Math.pow(isBachelorOrHigher * 100 - averageBachelor, 2);
             idSimTable.put(entry.getKey(), similarityvalue);
         }
@@ -234,14 +253,14 @@ public class OnlineCoursesAnalyzer {
         idSimTable = idSimTable.entrySet().stream().sorted(Map.Entry.comparingByValue())
             .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (s, s2) -> s,
                 LinkedHashMap::new));
-        List<String> numberList = idSimTable.entrySet().stream().sorted((e1,e2)-> {
+        List<String> numberList = idSimTable.entrySet().stream().sorted((e1, e2) -> {
             if (e1.getValue().equals(e2.getValue())) {
                 return idTitleTable.get(e1.getKey()).compareTo(idTitleTable.get(e2.getKey()));
-            }else {
+            } else {
                 return 0;
             }
         }).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (s, s2) -> s,
-            LinkedHashMap::new)).keySet().stream().toList();;
+            LinkedHashMap::new)).keySet().stream().toList();
         List<String> titleList = new ArrayList<>();
         for (String num : numberList) {
             titleList.add(idTitleTable.get(num));
@@ -291,14 +310,26 @@ class Course {
         this.institution = institution;
         this.number = number;
         this.launchDate = launchDate;
-        if (title.startsWith("\"")) title = title.substring(1);
-        if (title.endsWith("\"")) title = title.substring(0, title.length() - 1);
+        if (title.startsWith("\"")) {
+            title = title.substring(1);
+        }
+        if (title.endsWith("\"")) {
+            title = title.substring(0, title.length() - 1);
+        }
         this.title = title;
-        if (instructors.startsWith("\"")) instructors = instructors.substring(1);
-        if (instructors.endsWith("\"")) instructors = instructors.substring(0, instructors.length() - 1);
+        if (instructors.startsWith("\"")) {
+            instructors = instructors.substring(1);
+        }
+        if (instructors.endsWith("\"")) {
+            instructors = instructors.substring(0, instructors.length() - 1);
+        }
         this.instructors = instructors;
-        if (subject.startsWith("\"")) subject = subject.substring(1);
-        if (subject.endsWith("\"")) subject = subject.substring(0, subject.length() - 1);
+        if (subject.startsWith("\"")) {
+            subject = subject.substring(1);
+        }
+        if (subject.endsWith("\"")) {
+            subject = subject.substring(0, subject.length() - 1);
+        }
         this.subject = subject;
         this.year = year;
         this.honorCode = honorCode;
